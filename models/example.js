7ec811e5512f26aeae257ca472dd4c9412c734a9
Donald Park <donaldjpark@gmail.com>
2019-09-05 18:05:42 -0700

    ignore

--- models/example.js
+++ models/example.js
@@ -4,4 +4,25 @@ module.exports = function(sequelize, DataTypes) {
     description: DataTypes.TEXT
   });
   return Example;
+  
 };
+
+module.exports = function(sequelize, DataTypes) {
+  var main = sequelize.define("main", {
+    description: DataTypes.TEXT,
+    played: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false}
+  });
+  return main;
+}
+
+module.exports = function(sequelize, DataTypes) {
+  var play = sequelize.define("play", {
+    description: DataTypes.TEXT,
+    played: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false}
+  });
+  return play;
+}
--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,38 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+-- Main game DB --
+DROP DATABASE IF EXISTS project2;
+CREATE DATABASE project2;
+
+use project2;
+
+create table main
+(
+	id int not null AUTO_INCREMENT,
+	description tinytext,
+    played BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+create table play
+(
+	id int not null AUTO_INCREMENT,
+	description tinytext,
+    played BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO main (description) VALUES ('1');
+INSERT INTO main (description) VALUES ('2');
+INSERT INTO main (description) VALUES ('3');
+INSERT INTO main (description) VALUES ('4');
+INSERT INTO main (description) VALUES ('5');
+INSERT INTO main (description) VALUES ('6');
+INSERT INTO main (description) VALUES ('7');
+INSERT INTO main (description) VALUES ('8');
+INSERT INTO main (description) VALUES ('9');
+INSERT INTO main (description) VALUES ('10');
+
+INSERT INTO play (description) VALUES ('1'), ('2'), ('3'), ('4'), ('5'), ('6'), ('7'), ('8'), ('9'), ('10'), ('11'), ('12'), ('13'), ('14'), ('15'), ('16'), ('17'), ('18'), ('19'), ('20'), ('21'), ('22'), ('23');
