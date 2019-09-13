cb6850628a1f78fa0a7500f7a38ab350a4c3a1b3
morralex <morr.alex@outlook.com>
2019-09-12 20:26:53 -0700

    working chat

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
-//require web sockets (socket.io)
 var socket = require('socket.io');
 
 var db = require("./models");
@@ -12,16 +10,14 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// WE set up our app
+var server = app.listen(PORT, function () {
+    console.log("now listening on port " + PORT)
+});
+
+
+app.use(express.static("public"));
 
 
 // Routes
@@ -33,19 +29,15 @@ var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 
 // ---------- Socket Set Up ------------// 
 
-// WE set up our app
-var server = app.listen(PORT, function () {
-  console.log("now listening on port " + PORT)
-});
 
 var io = socket.listen(server);
 
-var sqlz = db.sequelize.sync(syncOptions);
+// var sqlz = db.sequelize.sync(syncOptions);
 // Starting the server, syncing our models ------------------------------------/
 // db.sequelize.sync(syncOptions).then(function () {
 //   io.on('connetion', function (socket) {
@@ -56,11 +48,22 @@ var sqlz = db.sequelize.sync(syncOptions);
 //   });
 // });
 
-io.on('connection', function (socket, sqlz) {
-  console.log(
-    ("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser." + PORT),
-    ("made a connection" + socket.id)
-  );
+io.on('connection', function (socket) {
+    console.log(
+        "==> 🌎  Listening on port" + PORT +
+        "\nmade a socket connection" + socket.id
+    );
+
+    //we want to get the data from the client 
+    socket.on('chat', function (data) {
+        //we want to grab the data entered by the client and send it to all the othe clients in the chat
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function (data) {
+        // we use boradcast, to show other clients sharing the server
+        socket.broadcast.emit('typing', data)
+    })
 })
 
 
