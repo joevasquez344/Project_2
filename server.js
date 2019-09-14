e294302609a8138c23e4aeb412ac8f9304775857
morralex <morr.alex@outlook.com>
2019-09-13 19:15:14 -0700

    tedious

--- server.js
+++ server.js
@@ -1,42 +1,8 @@
-// require("dotenv").config();
-// var express = require("express");
-// var exphbs = require("express-handlebars");
-// //require web sockets (socket.io)
+
+
 // // var socket = require('socket.io');
 // var io = require('socket.io').listen(server);
 
-
-// var db = require("./models");
-
-// var app = express();
-// var PORT = process.env.PORT || 3000;
-
-// // Middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-// app.use(express.static("public"));
-
-// // Handlebars
-// app.engine(
-//   "handlebars",
-//   exphbs({
-//     defaultLayout: "main"
-//   })
-// );
-// app.set("view engine", "handlebars");
-
-
-// // Routes
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
-
-// var syncOptions = { force: false };
-
-// // If running a test, set syncOptions.force to true
-// // clearing the `testdb`
-// if (process.env.NODE_ENV === "test") {
-//   syncOptions.force = true;
-// }
 
 // // ---------- Socket Set Up ------------// 
 // // WE set up our app
