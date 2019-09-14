5d92d0f2d2e5f91be3782225e6edd3671ee0ae1b
morralex <morr.alex@outlook.com>
2019-09-14 09:31:37 -0700

    add return

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -35,7 +35,7 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
   app.get("/", function(req, res) {
     if (req.user) {
-      res.redirect("/game");
+      return res.redirect("/game");
     }
     res.sendFile(path.join(__dirname, "../public/html/login.html"));
   });
