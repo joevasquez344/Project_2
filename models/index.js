4ea5f5e1b3f7399b428a3802a36aae0564eb1e5b
Donald Park <donaldjpark@gmail.com>
2019-09-14 00:29:40 -0700

    heroku

--- models/index.js
+++ models/index.js
@@ -15,7 +15,12 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config
+    config,
+    {
+        
+        dialect: "mysql"
+    }
+    
   );
 }
 
